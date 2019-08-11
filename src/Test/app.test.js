044d01febd905dbc20e9dd9b01c11b98f3965153
sarpong <abasimisarpong@rocketmail.com>
2019-08-11 21:37:33 +0100

    modified app test to correspond to rendering body

--- src/Test/app.test.js
+++ src/Test/app.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { App } from '../App';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import '../setupTests';
+import { Body } from '../components/body';
 
 describe('<App/>', ()=> {
   it('renders Hey', ()=>{
-    const wrapper = shallow(<App/>);
-    expect(wrapper.text()).toBe('hey')
+    const wrapper = mount(<App/>);
+    expect(wrapper.contains(<div><Body/></div>)).toBe(true);
+    wrapper.unmount()
   });
 });
