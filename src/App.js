553f291c017a8e7ca6e64ccba62719f253f6bf4c
sarpong <abasimisarpong@rocketmail.com>
2019-08-11 21:36:12 +0100

    App renders body component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Body } from '../src/components/body'
 
 export class App extends React.Component{
   render(){
     return(
       <div>
-      <h1 className='container'>hey</h1>
+      <Body/>
       </div>
     );
   };
