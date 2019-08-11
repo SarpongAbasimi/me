de0212111a5fc0f49c2779f75fde236fd1e4089b
sarpong <abasimisarpong@rocketmail.com>
2019-08-11 21:38:50 +0100

    wrote test for body component

---
+++ src/Test/body.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Body } from '../components/body';
+import { shallow } from 'enzyme';
+
+describe('<Body/>', ()=>{
+  it('renders a message', ()=>{
+    const wrapper = shallow(<Body/>)
+    expect(wrapper.text()).toBe('Hey, my name is Sarpong')
+  });
+});
