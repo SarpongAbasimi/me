0a734c6a1eb76131e9133eab8d3c7a0e1af0a928
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 17:04:42 +0100

    wrote test for wrapper component

--- src/Test/wrapper.test.js
+++ src/Test/wrapper.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Wrapper } from '../components/wrapper';
 import { shallow } from 'enzyme';
 import { Body } from '../components/body';
-import { Image } from '../components/image';
+import { Description } from '../components/description';
 
 describe('<Wrapper/>', ()=> {
   it('contains the image and body component', ()=>{
     const wrapper = shallow(<Wrapper/>);
     expect(wrapper.contains(<Body/>)).toBe(true);
-    expect(wrapper.contains(<Image/>)).toBe(false);
+    expect(wrapper.contains(<Description/>)).toBe(true);
   });
 });
