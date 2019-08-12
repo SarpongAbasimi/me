6b1cdc033786f9a3e8764f4d6847050f7db6d1ce
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 17:03:24 +0100

    description component

---
+++ src/components/description.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './description.css';
+
+export const Description = (props) => {
+  return(<div className='description'>
+    <span className='describo' role="img" aria-label="technologist">👨🏿‍💻 </span> 
+    <span className='describo'>Software Engineer at Makers.</span>
+  </div>);
+};
