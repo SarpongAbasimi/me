41a0205244979254b650acc2490ce3b727d7ae68
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 17:04:21 +0100

    added description to wrapper component

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Body } from '../components/body'
 import { Socials } from '../components/social';
+import { Description } from '../components/description';
 import './wrapper.css';
 
 export const Wrapper = (props)=> {
   return(
     <div className='wrapper'>
       <Body/>
+      <Description/>
       <Socials/>
     </div>
   );
