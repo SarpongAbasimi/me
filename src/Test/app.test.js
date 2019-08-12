57261467c4c97035ad80b4064924f299873258d9
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 12:54:14 +0100

    app test

--- src/Test/app.test.js
+++ src/Test/app.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { App } from '../App';
 import { mount } from 'enzyme';
 import '../setupTests';
-import { Body } from '../components/body';
+import { Wrapper } from '../components/wrapper';
 
 describe('<App/>', ()=> {
   it('renders Hey', ()=>{
     const wrapper = mount(<App/>);
-    expect(wrapper.contains(<div><Body/></div>)).toBe(true);
+    expect(wrapper.contains(<Wrapper/>)).toBe(true);
     wrapper.unmount()
   });
 });
