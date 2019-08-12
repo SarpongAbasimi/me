08f678cbad1ff0448379e9bb16f57cf54ce5788a
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 17:21:00 +0100

    Updates

---
+++ precache-manifest.df7b5c2132c8d74b1759e7415e2e50d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d5ff7f7156a56d28358a357ac6ae53",
+    "url": "/Profilo/index.html"
+  },
+  {
+    "revision": "daa61a2a72b0977c54a0",
+    "url": "/Profilo/static/css/main.5bf7b22c.chunk.css"
+  },
+  {
+    "revision": "3bebb8111b16d007ec33",
+    "url": "/Profilo/static/js/2.c3144ae8.chunk.js"
+  },
+  {
+    "revision": "daa61a2a72b0977c54a0",
+    "url": "/Profilo/static/js/main.5f2c355e.chunk.js"
+  },
+  {
+    "revision": "3006388d162dac4edec4",
+    "url": "/Profilo/static/js/runtime~main.9be7c56b.js"
+  }
+]);
