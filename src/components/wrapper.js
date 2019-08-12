77ce39f3246f12a69149f23d5c47b0e902b87c33
sarpong <abasimisarpong@rocketmail.com>
2019-08-12 16:17:24 +0100

    wrapper component

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Body } from '../components/body'
+import { Socials } from '../components/social';
 import './wrapper.css';
 
 export const Wrapper = (props)=> {
   return(
     <div className='wrapper'>
       <Body/>
+      <Socials/>
     </div>
   );
 };
